4bb43818db71a15bc0a39529e28e026130d400a4
Gabriel Lamarao <gabriel.lamarao97@gmail.com>
2022-03-14 11:23:49 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -5,10 +5,7 @@ import com.devsuperior.dsmovie.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/movies")
